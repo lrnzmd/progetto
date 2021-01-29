496189ef48c2ca506f27f15d32326b520939dc4f
lorenzo amodeo <lrnzmd@gmail>
2021-01-29 21:31:05 +0100

    fine ricerca, da risolvere bug cancellazione

--- src/App.js
+++ src/App.js
@@ -4,24 +4,46 @@ import Lista from './Lista';
 
 function App() {
 
-  const [oggetto, setOggetto] = useState([])
+  const [lista, setLista] = useState([])
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+
 
   useEffect(()=>{
     loadData()
-  },[])
+  },[]) 
+
+  const handleChange = (e) => {
+    const filtro = lista.filter(item => {
+      return (item.title.toLowerCase().includes(e.target.value.toLowerCase()) ||
+      item.body.toLowerCase().includes(e.target.value.toLowerCase())) 
+    });
+    console.log("test: ", filtro);
+    setSearchTerm(e.target.value);
+    setLista(filtro);
+    console.log(e.target.value)
+  }
+
 
   const loadData = async () =>{
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const dati = await response.json()
-    setOggetto(dati)
+    setLista(dati)
     console.log(dati)
   }
 
 
   return (
     <div className="App">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
   
-        <Lista oggetto={oggetto}/>
+      <Lista oggetto={lista}/>
     
     </div>
   );
