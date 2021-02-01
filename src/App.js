32231033aec42264398d80d47f5e78e8b8faffda
lorenzo amodeo <lrnzmd@gmail.com>
2021-02-01 01:38:30 +0100

    finita la logica

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   const [lista, setLista] = useState([])
   const [searchTerm, setSearchTerm] = useState("");
+  const [listafiltatta, setListafiltrata] = useState([])
 
 
 
@@ -14,7 +15,7 @@ function App() {
     loadData()
   },[]) 
 
-  const handleChange = (e) => {
+ /* const handleChange = (e) => {
     const filtro = lista.filter(item => {
       return (item.title.toLowerCase().includes(e.target.value.toLowerCase()) ||
       item.body.toLowerCase().includes(e.target.value.toLowerCase())) 
@@ -23,8 +24,17 @@ function App() {
     setSearchTerm(e.target.value);
     setLista(filtro);
     console.log(e.target.value)
-  }
+  }*/
 
+
+  useEffect(()=>{
+    setListafiltrata(
+      lista.filter(item => {
+        return (item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item.body.toLowerCase().includes(searchTerm.toLowerCase())) 
+      })
+    )
+  },[lista, searchTerm])
 
   const loadData = async () =>{
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
@@ -40,10 +50,10 @@ function App() {
         type="text"
         placeholder="Search"
         value={searchTerm}
-        onChange={handleChange}
+        onChange={(e)=> setSearchTerm(e.target.value)}
       />
   
-      <Lista oggetto={lista}/>
+      <Lista oggetto={listafiltatta}/>
     
     </div>
   );
